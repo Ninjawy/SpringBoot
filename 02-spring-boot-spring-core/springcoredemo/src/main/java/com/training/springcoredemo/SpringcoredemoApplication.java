2fbe2bb6e65723ae779e70db9a17f722bd98a7ba
Ninjawy <espon70@gmail.com>
2023-12-07 15:57:04 +0100

    until 40

--- 02-spring-boot-spring-core/springcoredemo/src/main/java/com/training/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/training/springcoredemo/SpringcoredemoApplication.java
@@ -11,7 +11,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  
  * @SpringBootApplication:  Bootstraps the Spring Boot application, creating 
  * 							application context and registering all beans, 
- * 							starts the embedded server Tomcat.. etc.
+ * 							starts the embedded server Tomcat
+ * So it works like this:
+ * 	Main Spring Boot application class automatically component scans package and
+ * 	sub-packages. Any other sub-packages we can give them any name under entity.
+ * 	Scans everything in com.<SpringPackagesPath>.<Project> recursively.
+ * 
+ * Common Pitfall - Different location for packages could be outside the project
+ * so they could be located somewhere else (e.g. com.training.util, 
+ * org.acme.cart, edu.cmu.srs), so by default Spring Boot will not 
+ * scan these packages. Only the packages of main Spring Boot application class
+ * and sub-packages could be scanned by default.
+ * 
+ * To explicitly list the packages to scan we can do as the following:
+ * 
+ * @SpringBootApplication(
+ * 		scanBasePackages={"com.training.util",
+ * 						  "org.acme.cart",
+ * 						  "edu.cmu.srs"})
+ * 
+ * instead of :
+ * @SpringBootApplication
  **/
 
 @SpringBootApplication 
