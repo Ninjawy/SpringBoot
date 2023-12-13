5a77fd75de99c79dcd98ec8b6079915f4700254c
Ninjawy <espon70@gmail.com>
2023-12-13 17:09:30 +0100

    section 3 - until setting up spring boot project

---
+++ 02-spring-boot-spring-core/03-cruddemoo-student/src/main/java/com/training/cruddemoo/CruddemooApplication.java
@@ -0,0 +1,19 @@
+package com.training.cruddemoo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemooApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String [] args) {
+		return runner -> System.out.println("Hello world!");
+	}
+}
